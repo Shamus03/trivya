1687ee4cb9c6818080049b46bfd0791758aaa7c8
Shamus03 <Shamus03@users.noreply.github.com>
2021-05-17 01:24:22 +0000

    deploy: e88e97e44de1d18c6c5c13d57e7f3394e56433f6

---
+++ precache-manifest.5f70d667344e761c659fc3facda62fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455513bc91eb5a4eff92",
+    "url": "/trivya/css/app.de4203eb.css"
+  },
+  {
+    "revision": "4acd8c4b02e70c212ad70307a953fb8e",
+    "url": "/trivya/index.html"
+  },
+  {
+    "revision": "455513bc91eb5a4eff92",
+    "url": "/trivya/js/app.b3e12f3e.js"
+  },
+  {
+    "revision": "76da0c8050fa2fe3844f",
+    "url": "/trivya/js/chunk-vendors.784f8768.js"
+  },
+  {
+    "revision": "3de88684a878807bd25a77bf2ea2b97a",
+    "url": "/trivya/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trivya/robots.txt"
+  }
+]);
