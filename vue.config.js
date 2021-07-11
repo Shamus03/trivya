54f2a9e4798cfa8c81046a7185c552f8257289ca
Shamus Taylor <Shamus03@me.com>
2021-07-11 18:17:37 -0500

    Update root dir (#1)
    * Update gh-pages.yml
    
    * Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  outputDir: 'dist/trivya',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/trivya/'
-    : '/',
   pwa: { 
     workboxOptions: {
       skipWaiting: false,
